1a0367ed08b3e80ae97e9f34bae2a86e07c4fb67
charchit319 <charchitcdahal527@gmail.com>
2025-08-22 10:15:33 +0545

    multiple Assets changed

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,35 +1,43 @@
-export default function test(){
-    const array1 = [
-    { name: "Card 1A", description: "First column card" },
-    { name: "Card 2A", description: "Another card in column 1" },
-    { name: "Card 3A", description: "Column 1 again" },
-  ];
+'use client'
+import Image from "next/image"
+import React,{useState} from "react";
 
-  const array2 = [
-    { name: "Card 1B", description: "First column 2 card" },
-    { name: "Card 2B", description: "Another card in column 2" },
-    { name: "Card 3B", description: "Column 2 again" },
-  ];
+export default function Test(){
+    const [count, setCount] = useState(0);
+    const handleadd = () => {
+        setCount(count + 1);
+    }
+    const handleSub = () => {
+        if(count > 0){
+            setCount(count - 1);
+        }
+    }
     return(
-        <div className="grid grid-cols-2 gap-4">
-  {/* Column 1 */}
-  <div className="flex flex-col gap-4">
-    {array1.map((item, idx) => (
-      <div key={idx} className="bg-white shadow p-4 rounded">
-        {item.name}
-      </div>
-    ))}
-  </div>
+        <div className="flex h-screen items-center justify-center">
+          <div className="flex w-[800px] h-[500px] bg-gradient-to-r from-[#FFFFFFE5] via-[#D8D8D8] to-[#D8D8D8] items-center justify-center rounded-2xl p-2">
+            <div className="flex bg-gradient-to-r from-[#FFFFFF66] to-[#ffffff00] rounded-2xl p-8 gap-4 w-full h-full">
+              <div className="flex flex-col gap-4 w-[50%]">
+                <h2 className="text-black text-center">Practice Session</h2>
+                <div className="flex flex-col gap-2">
+                  <div className="flex items-center space-x-10">
+                    <span className="ml-2 text-black">Measure sound</span>
+                    <button className="bg-amber-700 rounded-full p-4" onClick={()=>handleadd()}><Image src="assets/plus.svg" height={20} width={20} alt="just image"/></button>
+                    <span className="ml-2 text-black tabular-nums">{count}</span>
+                    <button className="bg-amber-700 rounded-full p-4" onClick={()=>handleSub()}><Image src="assets/minus.svg" height={20} width={20} alt="just image"/></button>
 
-  {/* Column 2 */}
-  <div className="flex flex-col gap-4">
-    {array2.map((item, idx) => (
-      <div key={idx} className="bg-white shadow p-4 rounded">
-        {item.name}
-      </div>
-    ))}
-  </div>
-</div>
+                  </div>
+                  
+                </div>
+
+              </div>
+              <div>
+
+              </div>
+
+            </div>
+
+          </div>
+        </div>
 
     )
 }
